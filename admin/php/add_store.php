6169217afe4ef22ae009f92672740913f5467cfa
HadM10 <hadmak10@gmail.com>
2024-02-11 20:41:47 +0200

    5th commit

--- admin/php/add_store.php
+++ admin/php/add_store.php
@@ -5,14 +5,15 @@ include('db_connection.php');
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
-    if (isset($_POST["store_name"]) && isset($_POST["category"]) && isset($_POST["description"])) {
+    if (isset($_POST["store_name"]) && isset($_POST["category_id"]) && isset($_POST["store_description"]) && isset($_POST["phone_number"])) {
         $storeName = $_POST["store_name"];
-        $category = $_POST["category"];
-        $description = $_POST["description"];
+        $category = $_POST["category_id"];
+        $description = $_POST["store_description"];
+        $phone = $_POST["phone_number"];
 
         // Perform data validation and database insertion
         // Example: Insert data into the 'stores' table
-        $insertQuery = "INSERT INTO stores (store_name, category, description) VALUES ('$storeName', '$category', '$description')";
+        $insertQuery = "INSERT INTO stores (store_name, store_description, category_id, phone_number) VALUES ('$storeName', '$description', '$category', '$phone')";
 
         if ($conn->query($insertQuery) === TRUE) {
             $response = array("status" => "success", "message" => "Store added successfully");

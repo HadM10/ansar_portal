a2bcc124947cdc0f225bb1efecf0a856fd715854
HadM10 <hadmak10@gmail.com>
2024-05-03 22:11:38 +0000

    18th commit

--- api/view_news.php
+++ api/view_news.php
@@ -15,11 +15,14 @@ $newsItems = array();
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
+        // Format the publication_date as desired (day month year)
+        $publication_date = date('d-m-Y', strtotime($row["publication_date"]));
+
         $newsItems[] = array(
             "news_id" => $row["news_id"],
             "title" => $row["title"],
             "content" => $row["content"],
-            "publication_date" => $row["publication_date"],
+            "publication_date" => $publication_date,
             "image_url" => $row["image_url"]
         );
     }
@@ -31,4 +34,5 @@ $conn->close();
 // Output JSON response
 header('Content-Type: application/json');
 echo json_encode($newsItems);
+
 ?>
--- api/view_offers.php
+++ api/view_offers.php
@@ -10,20 +10,25 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // Retrieve special offers information from the database
 $selectQuery = "SELECT o.offer_id, o.store_id, o.offer_title, o.offer_description, o.start_date, o.end_date, o.image_url, s.store_name
                 FROM offers o
-                INNER JOIN stores s ON o.store_id = s.store_id";
+                JOIN stores s ON o.store_id = s.store_id";
 $result = $conn->query($selectQuery);
 
 $offers = array();
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
+
+        $start_date = date('d-m-Y', strtotime($row["start_date"]));
+        $end_date = date('d-m-Y', strtotime($row["end_date"]));
+
         $offers[] = array(
             "offer_id" => $row["offer_id"],
-            "store_name" => $row["store_name"], // Change to store name
+            "store_id" => $row["store_id"],
+            "store_name" => $row["store_name"], // Include store name
             "offer_title" => $row["offer_title"],
             "offer_description" => $row["offer_description"],
-            "start_date" => $row["start_date"], // Include start date
-            "end_date" => $row["end_date"], // Include end date
+            "start_date" => $start_date,
+            "end_date" => $end_date,
             "image_url" => $row["image_url"]
         );
     }

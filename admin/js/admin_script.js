70c9511e95f1668762233904eefc16135b468fc0
HadM10 <hadmak10@gmail.com>
2024-05-15 14:40:37 +0000

    27th commit

--- admin/js/admin_script.js
+++ admin/js/admin_script.js
@@ -80,7 +80,7 @@ if (window.location.pathname.includes("login.php")) {
 
             if (response.message) {
               // Redirect to the admin panel or perform any other action on successful login
-              window.location.href = "https://ansarportal-deaa9ded50c7.herokuapp.com/admin/index.php";
+              window.location.href = "https://ansarportal-deaa9ded50c7.herokuapp.com/index.php";
             } else {
               alert(response.error);
             }
